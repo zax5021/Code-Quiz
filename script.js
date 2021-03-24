7f7c8055c7f0ff673b4a41ae5ac2b6eb4de328ea
zax5021 <zacharyacampbell1@gmail.com>
2021-03-23 22:33:48 -0700

    Done! added View HighScores and confirm prompt on clear highscores

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
+var headerEl = document.querySelector("header")
 var timerEl = document.querySelector("#timerCount");
 var startButton= document.querySelector(".start-button");
+var saveButton= document.querySelector("#save");
 var quizContentEl= document.querySelector(".quizContent");
 var startSectionEl= document.querySelector("#startSection");
+var scoresSectionEl= document.querySelector("#scoresSection");
+var initials= document.querySelector("#score-initials");
+var navHighscores= document.querySelector("#Highscores");
 
+
+var highScores = [];
+var storedScores = [];
 var isOver;
 var score = "";
 var chosenQuestion = "";
@@ -38,7 +46,6 @@ var questions = [
     },
 ]
 var qNumber = 0;
-console.log(questions);
 function shuffleArray() {
     for (var i = questions.length - 1; i > 0; i--) {
         var j = Math.floor(Math.random() * (i + 1));
@@ -47,20 +54,23 @@ function shuffleArray() {
         questions[j] = temp;
     }
 }
-
 var answerKey = [];
 var options =[];
 questions.forEach(function(element){
     answerKey=answerKey.concat(element.answer);
 })
-
 var li1 = document.createElement("li");
 var li2 = document.createElement("li");
 var li3 = document.createElement("li");
 var li4 = document.createElement("li");
 var questionTextEl = document.createElement("div");
 var optionsListEl = document.createElement("ol");
+var scoresListEl = document.createElement("ol");
 var scoresEl = document.createElement("p");
+var clearButton = document.createElement("button");
+clearButton.setAttribute("id", "clear-scores-btn");
+clearButton.textContent = "Clear Highscores";
+
 
 optionsListEl.setAttribute("style", "line-height:1.5; font-size: 18px; list-style-position: inside")
 console.log(questions.length)
@@ -112,7 +122,7 @@ function displayQuestion(){
         quizContentEl.appendChild(questionTextEl);
         quizContentEl.appendChild(optionsListEl);
 
-        questionTextEl.setAttribute("style", "margin-top: 50px; line-height:2")
+        questionTextEl.setAttribute("style", "margin-top: 50px;")
 
         optionsListEl.appendChild(li1);
         optionsListEl.appendChild(li2);
@@ -171,7 +181,6 @@ function countdown(){
         } if (isOver) {
             
             clearInterval(timeInterval);
-            timerEl.textContent = 0;
             gameOver();
         }
 
@@ -189,21 +198,106 @@ function gameOver () {
     if (isOver){
         score= timer
         quizContentEl.innerHTML = "";
-        questionTextEl.textContent = "Game over! See how your score compares by viewing highscores."
+        questionTextEl.textContent = "Game over! See how your score compares by clicking 'View Highscores' above."
         quizContentEl.appendChild(questionTextEl);
-        scoresEl.textContent = "Your Score was " + score ;
-        quizContentEl.appendChild(scoresEl);
+        if(score===0){
+            scoresEl.textContent = "Your Score was " + score + "... Refresh the page to try again." ;
+            quizContentEl.appendChild(scoresEl);
+        }   else {
+            scoresEl.textContent = "Your Score was " + score + "!!!" ;
+            quizContentEl.appendChild(scoresEl);
+            scoresSectionEl.setAttribute("style", "display:block")
+    } 
+}
+}
+function saveHighScores(){
+    var initUpper = initials.value
+  playerHighScore = {
+      currentScore: score,
+      scoreInitials: initUpper.toUpperCase()
+  };
+  storedScores.push(playerHighScore);
+  localStorage.setItem("globalHighScores", JSON.stringify(storedScores));
+}
+function renderTodos() { // the function that checks to see if there's any todo's stored in localStorage, and renders those on the page if so.
+  // TODO: Describe the functionality of the following two lines of code.
+  todoList.innerHTML = ""; // resetting html on unordered list.
+  todoCountSpan.textContent = todos.length; //setting the todo count to = the number of todos
+  
+  // TODO: Describe the functionality of the following `for` loop. // for each to do in the list, creating an element with the data-index of i, and creating the complete button and appending it to the html under todo-list. 
+  for (var i = 0; i < todos.length; i++) {
+    var todo = todos[i];
+
+    var li = document.createElement("li");
+    li.textContent = todo;
+    li.setAttribute("data-index", i);
+
+    var button = document.createElement("button");
+    button.textContent = "Complete ✔️";
+
+    li.appendChild(button);
+    todoList.appendChild(li);
+  }
+}
+
+
+
+function sortScores(){
+    storedScores.sort(function (a,b) {
+        return b.currentScore- a.currentScore;
+    });
+};
+
+function loadScores(){
+    quizContentEl.innerHTML= "";
+    scoresSectionEl.innerHTML= "";
+    headerEl.innerHTML= "";
+
+if (storedScores.length === 0) {
+    questionTextEl.textContent = "There are no High Scores yet... which means the leaderboard is wide open! Refresh the page to test your knowledge."
+    quizContentEl.appendChild(questionTextEl);
+} else {
+    questionTextEl.textContent = "The leaderboard, these are the best of the best. Refresh the page to test your knowledge."
+    quizContentEl.appendChild(questionTextEl);
+    quizContentEl.appendChild(scoresListEl);
+    for (var i=0; i<storedScores.length; i++) {
+    var topScore = storedScores[i];
+    var score_li = document.createElement("li")
+    score_li.textContent = "Initials: " + storedScores[i].scoreInitials + "          Score: " + storedScores[i].currentScore;
+    score_li.setAttribute("style", "width:80%; white-space:pre; list-style-position: inside");
+    score_li.setAttribute("class", "scores-list")
+    scoresListEl.appendChild(score_li);
     }
+    quizContentEl.appendChild(clearButton);
+}
+// alert("hello");
 }
 
 startButton.addEventListener("click", startGame);
 optionsListEl.addEventListener("click", answerquestion);
+saveButton.addEventListener("click", function(event) {
+    event.preventDefault();
+    saveHighScores();
+    sortScores ();
+    loadScores();
+    });
+clearButton.addEventListener("click", function(event) {
+    var result = confirm("Are you sure you want to clear scores? It's permanent...")
+    if (result){
+    event.preventDefault();
+    storedScores.length= 0;
+    localStorage.setItem("globalHighScores", JSON.stringify(storedScores));
+    loadScores();
+}});
+navHighscores.addEventListener("click", loadScores);
 
 function init () {
     shuffleArray(questions);
     qNumber=0
     isOver = false;
     timerEl.textContent= timer;
+    storedScores = JSON.parse(localStorage.getItem("globalHighScores"));
+    sortScores();
 }
 init();
 
