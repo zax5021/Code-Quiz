812dfacea07aa5f2b6ed8bd2ce66f221dd14ecdd
zax5021 <zacharyacampbell1@gmail.com>
2021-03-23 17:39:05 -0700

    variables are working, css is better

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var startButton= document.querySelector(".start-button");
 var quizContentEl= document.querySelector(".quizContent");
 var startSectionEl= document.querySelector("#startSection");
 
-
+var isOver;
 var timeLimitSeconds = 100;
 var highScore = "";
 var chosenQuestion = "";
@@ -50,14 +50,11 @@ function shuffleArray() {
     }
 }
 
-/*var questionsText = "";
-var chosenQuestion = "";*/
 var answerKey = [];
-var potentialAnswers =[];
+var options =[];
 questions.forEach(function(element){
     answerKey=answerKey.concat(element.answer);
 })
-console.log(answerKey)
 
 var li1 = document.createElement("li");
 var li2 = document.createElement("li");
@@ -67,105 +64,102 @@ var questionTextEl = document.createElement("div");
 var optionsListEl = document.createElement("ol");
 
 optionsListEl.setAttribute("style", "line-height:1.5; font-size: 18px; list-style-position: inside")
-
-
-
-console.log(questions);
-console.log(quizContentEl);
-console.log(questions[1].answer);
-
-
+console.log(questions.length)
 
 function getQuestion (){
-    if(qNumber > questions.length){
-        alert(gameover)
+    if(qNumber >= questions.length){
+        isOver= true;
         return;
     } else {
-    chosenQuestion = questions[qNumber];
-    // chosenQuestion = questions[Math.floor(Math.random() * questions.length)];
-    questionText = chosenQuestion.question;
-    delete chosenQuestion.question;
-    console.log("questionText: " + questionText);
-    console.log(chosenQuestion);
-    var options = Object.values(chosenQuestion);
-    console.log(typeof options);
-    function shuffle(options) {
-        var n = options.length, t, i;
-      
-        while (n) {
-      
-          i = Math.floor(Math.random() * n--);
-      
-          t = options[n];
-          options[n] = options[i];
-          options[i] = t;
+        chosenQuestion = questions[qNumber];
+        // chosenQuestion = questions[Math.floor(Math.random() * questions.length)];
+        questionText = chosenQuestion.question;
+        delete chosenQuestion.question
+        console.log("questionText: " + questionText);
+        options = Object.values(chosenQuestion);
+        console.log(typeof options);
+        function shuffle(options) {
+            var n = options.length, t, i;
+        
+            while (n) {
+        
+            i = Math.floor(Math.random() * n--);
+        
+            t = options[n];
+            options[n] = options[i];
+            options[i] = t;
+            }
+        
+            return ;
         }
-      
-        return ;
-      }
-      shuffle(options);
-      console.log(options);
-      potentialAnswers = options;
-      console.log("question number is :"+ qNumber)
-      qNumber = qNumber + 1;
-      console.log("question number is :"+ qNumber)
-    }}
+        shuffle(options);
+        console.log(options);
+        potentialAnswers = options;
+        console.log("question number is :"+ qNumber)
+        qNumber = qNumber + 1;
+        console.log("question number is :"+ qNumber)
+        }}
 
 function displayQuestion(){
-    console.log(potentialAnswers);
-    quizContentEl.innerHTML = "";
+    if (isOver){
+        return;
+    } else {
+        quizContentEl.innerHTML = "";
 
 
-    questionTextEl.textContent = questionText;
-    quizContentEl.appendChild(questionTextEl);
-    quizContentEl.appendChild(optionsListEl);
+        questionTextEl.textContent = questionText;
+        quizContentEl.appendChild(questionTextEl);
+        quizContentEl.appendChild(optionsListEl);
 
-    questionTextEl.setAttribute("style", "margin-top: 50px; line-height:2")
+        questionTextEl.setAttribute("style", "margin-top: 50px; line-height:2")
 
-    optionsListEl.appendChild(li1);
-    optionsListEl.appendChild(li2);
-    optionsListEl.appendChild(li3);
-    optionsListEl.appendChild(li4);
+        optionsListEl.appendChild(li1);
+        optionsListEl.appendChild(li2);
+        optionsListEl.appendChild(li3);
+        optionsListEl.appendChild(li4);
 
 
 
-    var lis = optionsListEl.querySelectorAll("li");
-    console.log(lis);
-    for (var i = 0; i < lis.length; i++) {
-       lis[i].textContent = potentialAnswers[i];
-       console.log(potentialAnswers[i])
-       lis[i].setAttribute("style", "margin-top: 5px; border:solid;color: white; background-color:grey; padding-left:10px; padding-right:10px; border-radius:25px; margin-left:25%; width: fit-content;")
-       if(answerKey.includes(potentialAnswers[i])){
-           lis[i].setAttribute("data-correct", "1")
-       } else {
-           lis[i].setAttribute("data-correct", "0")
-       }
-       console.log(potentialAnswers[i]);
-      }
+        var lis = optionsListEl.querySelectorAll("li");
+        console.log(lis);
+        for (var i = 0; i < lis.length; i++) {
+        lis[i].textContent = potentialAnswers[i];
+        console.log(potentialAnswers[i])
+        // lis[i].setAttribute("style", "margin-top: 5px; border:solid;color: white; background-color:grey; padding-left:10px; padding-right:10px; border-radius:25px; margin-left:25%; width: fit-content;")
+        if(answerKey.includes(potentialAnswers[i])){
+            lis[i].setAttribute("data-correct", "1")
+        } else {
+            lis[i].setAttribute("data-correct", "0")
+        }
+        console.log(potentialAnswers[i]);
+        }
         
-    };
+    }};
     //finish the wrong answer case (placeholder) and move to next question.
 function answerquestion(event){
     var selected = event.target;
     console.log(event.target);
    
     if (selected.matches("li")) {
-    var correct = selected.getAttribute("data-correct");
-        if (correct === "0"){
+        var correct = selected.getAttribute("data-correct");
+        if (correct !== "1"){
             alert("wrong")
         } 
-    // remove the selected question from the the possible choices and return to displayQuestion()
+        startGame();
 
     }
 }
 
 function startGame () {
-    isOver = false;
+    if (!isOver){
     startSectionEl.innerHTML = "";
     getQuestion();
     displayQuestion();
-    ;
-}
+    } else {
+    quizContentEl.innerHTML = "Game over!";
+
+    }
+};
 
 
 
@@ -174,6 +168,8 @@ optionsListEl.addEventListener("click", answerquestion);
 
 function init () {
     shuffleArray(questions);
+    qNumber=0
+    isOver = false;
 }
 init();
 
